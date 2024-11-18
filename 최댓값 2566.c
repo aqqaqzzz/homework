34f4ff64feaab3acf6b27ac756062c6cd8e8c049
황준원 <mango_1205@naver.com>
2024-11-18 22:20:38 +0900

    Add files via upload

---
+++ 최댓값 2566.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	int a[9][9];
+	int max = 0;
+	int max_x = 0; int max_y = 0;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			scanf_s("%d", &a[i][j]);
+			
+			if (a[i][j] > max) {
+				max = a[i][j];
+				max_x = a[i];
+				max_y = a[j];
+			}
+		}
+	}
+	printf("%d\n", max);
+	printf("%d %d\n", max_x + 1, max_y + 1);
+	return 0;
+}
