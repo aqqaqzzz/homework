03dfa39bbdda58f7e17bc730e6a0a915b4a6caa8
황준원 <mango_1205@naver.com>
2024-12-10 19:59:19 +0900

    Delete player.c

--- player.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "player.h"
-
-void inputPlayers(int p, Player players[]) {
-    for (int i = 0; i < p; i++) {
-        scanf("%d %s", &players[i].level, players[i].name);
-    }
-}
